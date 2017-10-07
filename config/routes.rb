99b59ea229237b5191220c1ed0acb4a58f4ada12
jimhotchkiss <jimbohtochkiss@yahoo.com>
2017-10-07 11:46:34 -0400

    Add user signup routes, form views, controller action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'projects#index'
 
+  get 'session/login', to: 'sessions#new'
+  post 'session/login', to: 'sessions#create'
+  delete 'session/logout', to: 'sessions#destroy'
+
   resources :users
   resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
