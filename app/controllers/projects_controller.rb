2c5861cf2944754fd704a2fb1bd3e59c2a8771b8
jimhotchkiss <jimbohtochkiss@yahoo.com>
2017-10-09 09:44:22 -0400

    Add project#new form, include checkbox categories, and unique category, project show view.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,34 @@
 class ProjectsController < ApplicationController
 
+  def index
+  end
+
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    binding.pry
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project = find_project
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, category_ids:[], categories_attributes: [:name])
+  end
+
+  def find_project
+    Project.find(params[:id])
   end 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+project_categories_list = ["home improvement", "auto maintinance", "painting", "electrical", "plumming", "carpentry", "appliance repair", "floor installation"]
+
+project_categories_list.each do |category|
+  Category.create(name: category)
+end
