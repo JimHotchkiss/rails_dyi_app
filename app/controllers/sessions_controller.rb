70feb322863e022464b3e5c007e9cef177197f2f
jimhotchkiss <jimbohtochkiss@yahoo.com>
2017-10-13 17:57:17 -0400

    Make corrections to session login and authentication messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,21 @@ class SessionsController < ApplicationController
       user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = user.id
       redirect_to root_path, notice: ', you are signed in'
-
     else
+
       @user = User.find_by(username: params[:username])
 
-      if @user
-        return redirect_to root_path unless @user.authenticate(params[:password])
+      if !@user
+        flash[:warning] = "Please check you information, and try again"
+        redirect_to root_path
+      elsif
+        @user.authenticate(params[:password])
         user_id = @user.id
         session[:user_id] = user_id
         redirect_to root_path, notice: ', you are signed in'
       else
-        redirect_to root_path, warning: 'you need to be signed up'
+        flash[:alert] = "Incorrect password"
+        render :new
       end
     end
 
